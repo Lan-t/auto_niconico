0b2794e69fbada946408adce20dc9bec55ec619f
tia <yknms3533642@yahoo.co.jp>
2019-08-08 12:09:39 +0900

    フルスクリーン機能追加

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from random import sample
 from itertools import cycle
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 from chromedriver_binary import add_chromedriver_to_path
 import requests
 
@@ -32,31 +32,42 @@ class AutoNiconico:
                 if p.netloc != 'account.nicovideo.jp':
                     break
 
-    def play(self, url, comment_off=True):
+    def play(self, url, comment_off=True, fullscreen=False):
         self.driver.get(url)
 
         self.click_play_pause_button()
 
-        try:
-            sleep(2)
-            self.driver.find_element_by_xpath('//*[@id="IMALinearView"]/div[1]/iframe')
-        except NoSuchElementException:
-            pass
-        else:
-            sleep(5)
-
-            try:
-                ad_frame = self.driver.find_element_by_xpath('//*[@id="IMALinearView"]/div[1]/iframe')
-                self.driver.switch_to.frame(ad_frame)
-                skip_button = self.driver.find_element_by_class_name('videoAdUiRedesignedSkipButton')
-                skip_button.click()
-            except NoSuchElementException:
-                sleep(2)
-            finally:
-                self.driver.switch_to.parent_frame()
+        self.skip_ad()
 
         if comment_off and self.comment_is_on():
-            self.click_comment_on_off_button()
+            self.must_click(self.click_comment_on_off_button)
+        if fullscreen and not self.fullscreen_is_on():
+            self.must_click(self.click_fullscreen_button)
+
+    def skip_ad(self, frame_timeout=3, click_timeout=3):
+        t = 0
+        while True:
+            try:
+                ad_frame = self.driver.find_element_by_xpath('//*[@id="IMALinearView"]/div[1]/iframe')
+            except NoSuchElementException:
+                sleep(0.5)
+                t += 0.5
+                if t >= frame_timeout:
+                    # print('フレームがない')
+                    return None
+                continue
+            break
+        self.driver.switch_to.frame(ad_frame)
+        try:
+            sleep(click_timeout)
+            skip_button = self.driver.find_element_by_class_name('videoAdUiRedesignedSkipButton')
+        except NoSuchElementException:
+            # print('スキップボタンない')
+            self.driver.switch_to.parent_frame()
+            return False
+        self.must_click(skip_button.click)
+        self.driver.switch_to.parent_frame()
+        return True
 
     def click_play_pause_button(self):
         try:
@@ -67,16 +78,39 @@ class AutoNiconico:
     def click_comment_on_off_button(self):
         self.driver.find_element_by_class_name('CommentOnOffButton').click()
 
+    def click_fullscreen_button(self):
+        try:
+            self.driver.find_element_by_class_name('DisableFullScreenButton').click()
+        except NoSuchElementException:
+            self.driver.find_element_by_class_name('EnableFullScreenButton').click()
+
+    def must_click(self, click_method):
+        while True:
+            try:
+                click_method()
+            except ElementNotInteractableException:
+                sleep(0.5)
+                continue
+            break
+
     def get_duration(self):
         duration_panel = self.driver.find_element_by_class_name('PlayerPlayTime-duration')
-        return strptime(duration_panel.text, '%M:%S')
+        try:
+            return strptime(duration_panel.get_attribute('innerHTML'), '%M:%S')
+        except ValueError:
+            print('error')
+            return strptime('59:59', '%M:%S')
 
     def get_playtime(self):
         playtime_panel = self.driver.find_element_by_class_name('PlayerPlayTime-playtime')
-        return strptime(playtime_panel.text, '%M:%S')
+        try:
+            return strptime(playtime_panel.get_attribute('innerHTML'), '%M:%S')
+        except ValueError:
+            print('error')
+            return strptime('00:00', '%M:%S')
 
     def video_is_ended(self):
-        return self.get_duration() < self.get_playtime()
+        return self.get_duration() <= self.get_playtime()
 
     def comment_is_on(self):
         try:
@@ -85,6 +119,14 @@ class AutoNiconico:
             return True
         else:
             return False
+
+    def fullscreen_is_on(self):
+        try:
+            self.driver.find_element_by_class_name('DisableFullScreenButton').click()
+        except NoSuchElementException:
+            return False
+        else:
+            return True
 
     def get_urls_from_ranking(self, url) -> List[str]:
         pr = parse.urlparse(url)
@@ -98,7 +140,7 @@ class AutoNiconico:
 
         return [i[link].text for i in items]
 
-    def play_ranking(self, url, loop=True, shuffle=False, comment_off=True):
+    def play_ranking(self, url, loop=True, shuffle=False, comment_off=True, fullscreen=True):
         urls = self.get_urls_from_ranking(url)
 
         playlist_p = range(len(urls))
@@ -115,7 +157,7 @@ class AutoNiconico:
             except StopIteration:
                 break
 
-            self.play(url_p, comment_off=comment_off)
+            self.play(url_p, comment_off=comment_off, fullscreen=fullscreen)
 
             while not self.video_is_ended():
                 sleep(0.9)
@@ -128,13 +170,14 @@ if __name__ == '__main__':
         url = sys.argv[1]
     except IndexError:
         sys.stderr.write(
-            f'Usage: python {sys.argv[0]} ランキングのURL [-l --loop] [-s --shuffle] [-c --no-comment] [-a --login] [--user=username] [--password=password]')
+            f'Usage: python {sys.argv[0]} ランキングのURL [-l --loop] [-s --shuffle] [-c --no-comment] [-f --fullscreen] [-a --login] [--user=username] [--password=password]')
         exit(1)
     argv = set(sys.argv)
     loop = '--loop' in argv or '-l' in argv
     shuffle = '--shuffle' in argv or '-s' in argv
     no_comment = '--no-comment' in argv or '-c' in argv
     login = '--login' in argv or '-a' in argv
+    fullscreen = '--fullscreen' in argv or '-f' in argv
     user = ''
     password = ''
     for arg in sys.argv:
@@ -148,4 +191,4 @@ if __name__ == '__main__':
     niconico = AutoNiconico()
     if login:
         niconico.login(username=user, password=password)
-    niconico.play_ranking(url, loop=loop, shuffle=shuffle, comment_off=no_comment)
+    niconico.play_ranking(url, loop=loop, shuffle=shuffle, comment_off=no_comment, fullscreen=fullscreen)
