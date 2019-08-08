3cbe6b912b614aa7a5a7ed999543e9eeb6d67ffa
tia <yknms3533642@yahoo.co.jp>
2019-08-09 02:59:30 +0900

    オプションの指定の仕方変えた、タグ検索できるようにした

--- README.md
+++ README.md
@@ -4,8 +4,13 @@
   
 
 Usage: 
-最初だけ `pip install -r requirements.txt`  
-`$ python main.py ランキングのURL [-l --loop] [-s --shuffle] [-c --no-comment] [-f --fullscreen] [-a --login] [--user=username] [--password=password]`  
+最初だけ `$ pip install -r requirements.txt`  
+`python main.py ([-u --url=URL] | [-t --tag=TAG]) [-lscfa] [-U --user=username] [-P --password=password]`  
+`-l --loop`
+`-s --shuffle`
+`-c --no-comment`
+`-f --fullscreen`
+`-a --login`
 
 --loginすると最初にログイン画面が開く  
 --user, --passwordを両方指定すると自動でログインする(パスワード平文は怖いのでおすすめしない)  
@@ -15,5 +20,7 @@ Usage:
 シークバーとかも動かして大丈夫  
 
 ランキング以外でもクエリにrss=2.0ってつけるとxmlが振ってくるタイプのやつならなんでも
+--tagはタグ検索、ソートとかしたかったら検索画面のURLを--urlで指定
+タグは空白で挟んで複数指定可能（"なり'で挟まないとだめ (--tag="初音ミク MMD")）
 
 終わるときはブラウザ閉じる(コンソールはエラー吐くけど気にしない)  
--- main.py
+++ main.py
@@ -215,30 +215,80 @@ class AutoNiconico:
 
 if __name__ == '__main__':
     import sys
+    from collections import defaultdict
 
-    try:
-        url = sys.argv[1]
-    except IndexError:
+    if len(sys.argv) == 1:
         sys.stderr.write(
-            f'Usage: python {sys.argv[0]} ランキングのURL [-l --loop] [-s --shuffle] [-c --no-comment] [-f --fullscreen] [-a --login] [--user=username] [--password=password]')
+            f'Usage: '
+            f'$ python {sys.argv[0]} ([-u --url=URL] | [-t --tag=TAG]) [-lscfa] '
+            f'[-U --user=username] [-P --password=password]\n'
+            f'Options:\n'
+            f'  -l --loop\n'
+            f'  -s --shuffle\n'
+            f'  -c --no-comment\n'
+            f'  -f --fullscreen\n'
+            f'  -a --login\n')
         exit(1)
-    argv = set(sys.argv)
-    loop = '--loop' in argv or '-l' in argv
-    shuffle = '--shuffle' in argv or '-s' in argv
-    no_comment = '--no-comment' in argv or '-c' in argv
-    fullscreen = '--fullscreen' in argv or '-f' in argv
-    login = '--login' in argv or '-a' in argv
-    user = ''
-    password = ''
-    for arg in sys.argv:
-        if arg[:7] == '--user=':
-            user = arg[7:]
-            login = True
-        if arg[:11] == '--password=':
-            password = arg[11:]
-            login = True
+
+    options = []
+    prop = defaultdict(str)
+    for i in sys.argv:
+        if i[:2] == '--':
+            if '=' in i:
+                k, v = i[2:].split('=', 1)
+                if len(k) == 1:
+                    sys.stderr.write(f'--{k}= option is invalid. did you mean -{k}= ?\n')
+                    exit(1)
+                prop[k] = v.strip('"\'')
+            else:
+                options.append(i[2:])
+            continue
+
+        if i[:1] == '-':
+            if '=' in i:
+                k, v = i[1:].split('=', 1)
+                if len(k) != 1:
+                    sys.stderr.write(f'-{k}= option is invalid. did you mean --{k}= ?\n')
+                    exit(1)
+                prop[k] = v.strip('"\'')
+            else:
+                for j in i[1:]:
+                    options.append(j)
+                    continue
+    argv = set(options)
+
+    loop = 'loop' in argv or 'l' in argv
+    shuffle = 'shuffle' in argv or 's' in argv
+    no_comment = 'no-comment' in argv or 'c' in argv
+    fullscreen = 'fullscreen' in argv or 'f' in argv
+    login = 'login' in argv or 'a' in argv
+
+    if prop['url'] and prop['u']:
+        sys.stderr.write('--url と -u が同時に指定されています\n')
+        exit(1)
+    prop['url'] = prop['url'] or prop['u']
+
+    if prop['tag'] and prop['t']:
+        sys.stderr.write('--tag と -t が同時に指定されています\n')
+        exit(1)
+    prop['tag'] = prop['tag'] or prop['t']
+
+    if prop['user'] and prop['U']:
+        sys.stderr.write('--user と -U が同時に指定されています\n')
+        exit(1)
+    prop['user'] = prop['user'] or prop['U']
+
+    if prop['password'] and prop['P']:
+        sys.stderr.write('--password と -P が同時に指定されています\n')
+        exit(1)
+    prop['password'] = prop['password'] or prop['P']
+
+    if prop['url'] and prop['tag']:
+        sys.stderr.write('urlとタグが同時に指定されています\n')
+        exit(1)
+    prop['url'] = prop['url'] or 'https://www.nicovideo.jp/tag/' + prop['tag']
 
     niconico = AutoNiconico()
-    if login:
-        niconico.login(username=user, password=password)
-    niconico.play_anylist(url, loop=loop, shuffle=shuffle, comment_off=no_comment, fullscreen=fullscreen)
+    if login or prop['user'] or prop['password']:
+        niconico.login(username=prop['user'], password=prop['password'])
+    niconico.play_anylist(prop['url'], loop=loop, shuffle=shuffle, comment_off=no_comment, fullscreen=fullscreen)
